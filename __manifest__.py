b132ef0a6db19fd5f1eae8887693d60b5f339f5c
Tudor Trica-Mavrodin <tudor.trica-mavrodin@mail.mcgill.ca>
2025-01-17 17:24:14 -0500

    progress is progressing

--- __manifest__.py
+++ __manifest__.py
@@ -1,15 +1,16 @@
 {
-    'name': 'Radoo'
+    'name': 'Radoo',
     'version': '1.0.0',
     'author': 'Radish Cooperative',
     'summary': 'Fast, affordable local delivery with Radish!',
     'category': 'eCommerce',
-    'website': 'https://radsish.coop',
-    'license': 'GPL-3',
+    'website': 'https://radish.coop',
     'depends': ['website_sale', 'sale', 'stock', 'delivery'],
     'data': [
-        'views/shipping_views.xml',
-        'security/ir.model.access.csv',
+        # 'views/shipping_views.xml',
+        # 'security/ir.model.access.csv',
     ],
     'installable': True, 
+    'application': True,
+    'license': 'GPL-3',
 }
